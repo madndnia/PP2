2dd90fc25222a33ea115c2f4f950e8b286d9aebb
Madina <nurma2005@bk.ru>
2024-01-30 15:48:16 +0600

    Dictionaries

---
+++ Lab2/6.py
@@ -0,0 +1,54 @@
+#Python Dictionaries
+
+#EXAMPLES:
+
+#Create and print a dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#Print the "brand" value of the dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+
+#Duplicate values will overwrite existing values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+
+#String, int, boolean, and list data types:
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+
+#Print the data type of a dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+
+#Using the dict() method to make a dictionary:
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+
