b394e282ddcc4d314632517a3fb6492d0632cc0b
Madina <nurma2005@bk.ru>
2024-02-01 01:32:52 +0600

    For Loops

--- Lab2/9.py
+++ Lab2/9.py
@@ -79,3 +79,34 @@ for x in adj:
 for x in [0, 1, 2]:
   pass
 
+
+#EXERCISES:
+
+#Loop through the items in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+
+  print(x)
+
+
+#In the loop, when the item value is "banana", jump directly to the next item.
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+
+  print(x)
+
+
+#Use the range function to loop through a code set 6 times.
+for x in range(6):
+  print(x)
+
+
+#Exit the loop when x is "banana".
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+
+  print(x)
