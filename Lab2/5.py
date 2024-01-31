d4c0193501d43253f586550f09aad4760737ade7
Madina <nurma2005@bk.ru>
2024-02-01 00:52:36 +0600

    Sets

--- Lab2/5.py
+++ Lab2/5.py
@@ -51,6 +51,37 @@ print(thisset)
 
 
 
+#EXERCISES
+
+#Use the add method to add "orange" to the fruits set.
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+
+#Use the correct method to add multiple items (more_fruits) to the fruits set.
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+
+#Use the remove method to remove "banana" from the fruits set.
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+
+#fruits = {"apple", "banana", "cherry"}
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+
+
+
+
+
+
+
+
+
 
 
 
